5bcbd0a6342eecd160d2b09d244f38428bc5eae4
tiany0321 <tianyuan210@163.com>
2025-03-11 15:06:25 +0800

    MOAF

---
+++ MAZ/repeat/tolerance0.003/MOAF/size0.1/size0.1.R
@@ -0,0 +1,453 @@
+setwd("C:\\Users\\12758\\Desktop\\target_oriented_BO\\MAZ\\repeat\\tolerance0.003\\MOAF\\size0.1")
+start_time<-Sys.time()
+library(rPref)
+library(dplyr)
+library("DiceKriging")
+
+fn.utility=function(data.training,estimator,training.data,infj,tar_num){
+ 
+   #EGO
+  if (estimator==1){ 
+    fn.ego.ei = function(training.data,infj)
+    {
+      ego = (min(training.data[,"es"]) - infj[,"mean"])/infj[,"sd"]
+      z = ego
+      ei.ego = infj[,"sd"]*z*pnorm(z) + infj[,"sd"]*dnorm(z) 
+      ei.ego = data.frame(ei.ego)
+      return (ei.ego)
+    }
+    EI= fn.ego.ei(training.data,infj)
+    EI[which(EI[,]=="NaN"),]=0
+    EI=as.data.frame(EI)
+    colnames(EI)="EI"
+  }
+  
+  #PI
+  if (estimator==2){ 
+    fn.PI.ei = function(training.data,infj)
+    {
+      PI = (min(training.data[,"es"]) - infj[,"mean"])/infj[,"sd"]
+      z = PI
+      ei.PI = pnorm(z) 
+      ei.PI = data.frame(ei.PI)
+      return (ei.PI)
+    }
+    EI= fn.PI.ei(training.data,infj)
+    EI[which(EI[,]=="NaN"),]=0
+    EI=as.data.frame(EI)
+    colnames(EI)="EI"
+  }
+  
+  #LCB
+  if (estimator==3){ 
+    fn.LCB = function(training.data,infj,f=0.01,delta=0.1)
+    {
+      LCB = infj[,"mean"] -(2*f*log10(nrow(training.data)*(nrow(training.data)-size.i+1)^2*3.14^2/(6*delta)))^0.5*infj[,"sd"]
+      #  LCB = infj[,"mean"] -1*infj[,"sd"]
+      LCB = data.frame(LCB)
+      return (LCB)
+    }
+    LCB.v= fn.LCB(training.data,infj,f=0.01,delta=0.1)################parameter delta
+    LCB.v=as.data.frame(LCB.v)
+    LCB.v[which(LCB.v[,]=="NaN"),]=0
+    
+    EI=as.data.frame(-LCB.v)
+    colnames(EI)="EI"
+  }
+  
+  #EQI
+  if (estimator==4){ 
+    fn.EQI.ei = function(training.data,infj,eqi.beta=0.85,epsilon=(10^(-5))^0.5)
+    {
+      s1=infj[,"sd"]^2/(epsilon^2+infj[,"sd"]^2)^(1/2)#
+      s2=infj[,"sd"]*epsilon/(epsilon^2+infj[,"sd"]^2)^(1/2)#
+      q.min=min(infj[,"mean"]+qnorm(eqi.beta)*infj[,"sd"])#
+      
+      EQI = (q.min-(infj[,"mean"] + qnorm(eqi.beta)*s2))/s1#
+      z=EQI
+      ei.EQI = s1*z*pnorm(z) + s1*dnorm(z)  #
+      
+      ei.EQI = data.frame(ei.EQI)
+      return (ei.EQI)
+    }
+    EI= fn.EQI.ei(training.data,infj)
+    EI[which(EI[,]=="NaN"),]=0
+    EI=as.data.frame(EI)
+    colnames(EI)="EI"
+  }
+  
+  #MOAF
+  if (estimator==5){ 
+    fn.ego.ei = function(training.data,infj)
+    {
+      ego = (min(training.data[,"es"]) - infj[,"mean"])/infj[,"sd"]
+      z = ego
+      ei.ego = infj[,"sd"]*z*pnorm(z) + infj[,"sd"]*dnorm(z) 
+      ei.ego = data.frame(ei.ego)
+      return (ei.ego)
+    }
+    ego= fn.ego.ei(training.data,infj)
+    ego[which(ego[,]=="NaN"),]=0
+    ego=as.data.frame(ego)
+    colnames(ego)="ego"
+    
+    fn.PI.ei = function(training.data,infj)
+    {
+      PI = (min(training.data[,"es"]) - infj[,"mean"])/infj[,"sd"]
+      z = PI
+      ei.PI = pnorm(z) 
+      ei.PI = data.frame(ei.PI)
+      return (ei.PI)
+    }
+    PI= fn.PI.ei(training.data,infj)
+    PI[which(PI[,]=="NaN"),]=0
+    PI=as.data.frame(PI)
+    colnames(PI)="PI"
+    
+    fn.LCB = function(training.data,infj,f=0.01,delta=0.1)
+    {
+      LCB = infj[,"mean"] -(2*f*log10(nrow(training.data)*(nrow(training.data)-size.i+1)^2*3.14^2/(6*delta)))^0.5*infj[,"sd"]
+      #  LCB = infj[,"mean"] -1*infj[,"sd"]
+      LCB = data.frame(LCB)
+      return (LCB)
+    }
+    LCB.v= fn.LCB(training.data,infj,f=0.01,delta=0.1)################parameter delta
+    LCB.v=as.data.frame(LCB.v)
+    LCB.v[which(LCB.v[,]=="NaN"),]=0
+    
+    LCB=as.data.frame(-LCB.v)
+    colnames(LCB)="LCB"
+    
+    M_score=cbind(ego,PI,LCB)
+    M_score=as.data.frame(scale(M_score))
+    # 计算 Pareto 前沿（目标是最大化）
+    pareto_front <- psel(M_score, high(ego) * high(PI) * high(LCB))
+    
+    # 计算 Pareto 前沿上每个点的目标值的平均值
+    pareto_front <- pareto_front %>%
+      mutate(mean_value = (ego + PI + LCB) / 3)
+    
+    # 找到与平均值最接近的点
+    middle_point <- pareto_front %>%
+      slice(which.min(abs(mean_value - median(mean_value))))
+    
+    # 输出最中间点的索引
+    middle_index <- which(M_score$ego == middle_point$ego &
+                            M_score$PI == middle_point$PI &
+                            M_score$LCB == middle_point$LCB)
+    EI=as.data.frame(rep(0,nrow(infj)))
+    colnames(EI)="EI"
+    EI[middle_index,]=1
+  }
+  
+  #PureExp
+  if(estimator==6)
+  { 
+    fn.max.u.ei = function(infj)
+    {
+      max.u.ei = -abs(infj[,"mean"]- data.training[tar_num,"es"])
+      return (max.u.ei)
+    }
+    EI= fn.max.u.ei(infj)
+    EI=as.data.frame(EI)
+    EI[which(EI[,]=="NaN"),]=0
+    colnames(EI)="EI"
+  }
+  
+  #t-EGO
+  if (estimator==7)
+  { 
+    fn.ego.ei = function(data.training,training.data,infj,para=  data.training[tar_num,"es"])
+    {
+      if(tar_num%in%which(data.training[,"es"]==max(data.training[,"es"])))
+      {
+        para=Inf
+        yit=max(training.data[,"es"])
+        w1=((2*para-yit) - infj[,"mean"])/infj[,"sd"]
+        w2=(infj[,"mean"] - yit  )/infj[,"sd"]
+        w3=( para - infj[,"mean"] )/infj[,"sd"]
+        w4=( infj[,"mean"] - para )/infj[,"sd"]
+        
+        # w1_ego=infj[,"sd"]*w1*pnorm(w1) + infj[,"sd"]*dnorm(w1) 
+        w2_ego=infj[,"sd"]*w2*pnorm(w2) + infj[,"sd"]*dnorm(w2) 
+        #  w3_ego=infj[,"sd"]*w1*pnorm(w3) + infj[,"sd"]*dnorm(w3) 
+        #   w4_ego=infj[,"sd"]*w2*pnorm(w4) + infj[,"sd"]*dnorm(w4) 
+        EI=w2_ego
+        #ei.ego=w1_ego-w3_ego+w2_ego-w4_ego
+        # print((para-min(abs(training.data[,"es"]-para)))%in%training.data[,"es"])
+      }else if(tar_num%in%which(data.training[,"es"]==min(data.training[,"es"]))){
+        {
+          para=Inf
+          yit=min(training.data[,"es"])
+          w1=( yit - infj[,"mean"] )/infj[,"sd"]
+          w2=(infj[,"mean"] - (2*para-yit))/infj[,"sd"]
+          w3=( para - infj[,"mean"] )/infj[,"sd"]
+          w4=( infj[,"mean"] - para )/infj[,"sd"]
+          
+          # w1_ego=infj[,"sd"]*w1*pnorm(w1) + infj[,"sd"]*dnorm(w1) 
+          w1_ego=infj[,"sd"]*w1*pnorm(w1) + infj[,"sd"]*dnorm(w1) 
+          #  w3_ego=infj[,"sd"]*w1*pnorm(w3) + infj[,"sd"]*dnorm(w3) 
+          #   w4_ego=infj[,"sd"]*w2*pnorm(w4) + infj[,"sd"]*dnorm(w4) 
+          EI=w1_ego
+          #ei.ego=w1_ego-w3_ego+w2_ego-w4_ego
+          # print((para-min(abs(training.data[,"es"]-para)))%in%training.data[,"es"])
+        }
+      }else{
+        if(signif((min(abs(training.data[,"es"]-para))+para),10) %in% signif(training.data[,"es"],10))
+        {
+          yit=signif((min(abs(training.data[,"es"]-para))+para),10)
+          w1=( yit - infj[,"mean"] )/infj[,"sd"]
+          w2=(infj[,"mean"] - (2*para-yit))/infj[,"sd"]
+          w3=( para - infj[,"mean"] )/infj[,"sd"]
+          w4=( infj[,"mean"] - para )/infj[,"sd"]
+          
+          w1_ego=infj[,"sd"]*w1*pnorm(w1) + infj[,"sd"]*dnorm(w1) 
+          w2_ego=infj[,"sd"]*w2*pnorm(w2) + infj[,"sd"]*dnorm(w2) 
+          w3_ego=infj[,"sd"]*w1*pnorm(w3) + infj[,"sd"]*dnorm(w3) 
+          w4_ego=infj[,"sd"]*w2*pnorm(w4) + infj[,"sd"]*dnorm(w4) 
+          
+          EI=w1_ego-w3_ego+w2_ego-w4_ego
+          # print((min(abs(training.data[,"es"]-para))+para)%in%training.data[,"es"])
+        }
+        if(signif((para-min(abs(training.data[,"es"]-para))),10) %in% signif(training.data[,"es"],10))
+        {
+          yit=signif((para-min(abs(training.data[,"es"]-para))),10)
+          w1=((2*para-yit) - infj[,"mean"])/infj[,"sd"]
+          w2=(infj[,"mean"] - yit  )/infj[,"sd"]
+          w3=( para - infj[,"mean"] )/infj[,"sd"]
+          w4=( infj[,"mean"] - para )/infj[,"sd"]
+          
+          w1_ego=infj[,"sd"]*w1*pnorm(w1) + infj[,"sd"]*dnorm(w1) 
+          w2_ego=infj[,"sd"]*w2*pnorm(w2) + infj[,"sd"]*dnorm(w2) 
+          w3_ego=infj[,"sd"]*w1*pnorm(w3) + infj[,"sd"]*dnorm(w3) 
+          w4_ego=infj[,"sd"]*w2*pnorm(w4) + infj[,"sd"]*dnorm(w4) 
+          
+          EI=w1_ego-w3_ego+w2_ego-w4_ego
+          # print((para-min(abs(training.data[,"es"]-para)))%in%training.data[,"es"])
+        }
+      }
+      ei.ego = data.frame(EI)
+      return (ei.ego)
+    }
+    EI= fn.ego.ei(data.training,training.data,infj,para=  data.training[tar_num,"es"])
+    EI[which(EI[,]=="NaN"),]=0
+    colnames(EI)="EI"
+  }
+  
+  return(EI)
+}
+
+fn.gp = function(training.data.x, training.data.y,virtual.data.x)
+{
+  set.seed(23)
+  noise=rep(0.02*(max(training.data.y)-min(training.data.y)),nrow(training.data.x))
+  m<-  km(formula<-~.,design=training.data.x,noise.var = noise, response=training.data.y,covtype = "exp",optim.method="BFGS")
+  set.seed(23)
+  p <- predict(m, virtual.data.x, "UK")
+  sd=as.data.frame(p$sd)
+  mean=as.data.frame(p$mean)
+  pr=cbind(mean,sd)
+  colnames(pr)=c("mean","sd")
+  return(pr)
+}
+
+fn.selector = function(EI){
+  num=which(EI[,]==max(EI[,]))
+  return(num)
+}
+
+fn.OC = function(realdata,traindata){
+  OC=min(abs(realdata[tar_num,"es"]-traindata[,"es"]))
+  return(OC)
+}
+
+fn.dis=function(realdata,pre_actual){
+  dis=min(abs(realdata[tar_num,"es"]-pre_actual))
+  return(dis)
+}
+
+
+data.3D=read.csv("MA2Z4_all.csv")
+data.3D=na.omit(data.3D)
+data.training=data.3D
+colnames(data.training)[3]="es"
+order.data=as.data.frame(rep(1:nrow(data.training)))
+colnames(order.data)="order.num"
+data.training=cbind(data.training,order.data)
+keep=c("es", "AverW_Boi","Aver_Ros","Max_Homo","Aver_Pc","Min_Lumo")
+keep.x=c("AverW_Boi","Aver_Ros","Max_Homo","Aver_Pc","Min_Lumo")
+data.training=data.frame(data.training[,keep]) 
+
+data.training_order=data.training[order(-data.training[,"es"]),]
+data.training=cbind(data.training_order,order.data)
+y_range=max(data.training[,"es"])-min(data.training[,"es"])
+
+tar=c(112)
+data.training[,"es"]=abs(data.training[,"es"]-data.training[tar,"es"])
+
+train.num=200
+iter=seq(0,50)
+
+iter=as.data.frame(iter)
+SD=matrix(0,nrow(iter)-1,1)
+dis=matrix(0,nrow(iter)-1,1)
+data.OC.SD=matrix(0,nrow(iter)-1,train.num)
+data.dis=matrix(0,nrow(iter)-1,train.num)
+data.iter.num.max=matrix(,train.num,1)
+size.i=c(round(0.1*nrow(data.training)))
+
+iter_total=matrix(,1,10)
+iter_mean=matrix(0,train.num,1)
+iter_mean_w=matrix(0,train.num,1)
+ave.OC=matrix(,nrow(iter)-1,10)
+ave.dis=matrix(,nrow(iter)-1,10)
+ave.OC_total=matrix(,nrow(iter)-1,10)
+ave.dis_total=matrix(,nrow(iter)-1,10)
+ave.OC_mean=matrix(0,nrow(iter)-1,1)
+ave.dis_mean=matrix(0,nrow(iter)-1,1)
+set.seed(11*size.i)
+sample.num0<-vector()
+for(ii in 1:1000){
+  s <-  sample(c(1:106,118:(nrow(data.training))),80,replace=F,prob=NULL) 
+  sample.num0<-as.data.frame(rbind(sample.num0,s))
+}
+
+estimator=5
+
+repeat{
+  SD=matrix(0,nrow(iter)-1,1)
+  dis=matrix(0,nrow(iter)-1,1)
+  data.OC.SD=matrix(0,nrow(iter)-1,train.num)
+  data.dis=matrix(0,nrow(iter)-1,train.num)
+  tar_num = tar
+
+  for(size in size.i)
+  {
+    sample.num<-sample.num0[,1:(size)]
+    for(i in 1:train.num)
+    {
+      
+      split=sample.num[i,]
+      split=unlist(split)
+      training.data <- data.training[split,]
+      training.data.x <- data.frame(training.data[,keep.x]) 
+      training.data.y <-  data.frame(training.data[,"es"]) 
+      colnames(training.data.y)="es"
+      vir.total.data.x=data.frame(data.training[,keep.x]) 
+      data.all=data.frame(data.training) 
+     
+      virtual.data <- data.training[-split,]
+      rownames(virtual.data)<-1:nrow(virtual.data)
+      virtual.data.x=as.data.frame(virtual.data[,keep.x])
+      gp = fn.gp(training.data.x, training.data.y,virtual.data.x)
+      
+      EI0= fn.utility(data.training,estimator,training.data,gp,tar_num)
+      EI0=as.data.frame(EI0)
+      ######
+      
+      if(min(abs(training.data[,"es"]-data.training[tar_num,"es"]))<=0.003*(y_range))
+      {
+        SD[c(1:(nrow(iter)-1)),]=0 
+        dis[c(1:(nrow(iter)-1)),]=0 
+        u=0
+      }else{
+        
+        SD[1,1]= fn.OC(data.training,training.data)
+        dis[1,1]= fn.OC(data.training,training.data)
+        
+        num=min(virtual.data[which(EI0[,"EI"]==max(EI0[,"EI"])),"order.num"])
+        
+        training.data0=training.data
+        virtual.data0=virtual.data
+        
+        ####
+        u=1
+        repeat{
+          training.data0[nrow(training.data0)+1,]=virtual.data0[which(virtual.data0[,"order.num"]==num),]
+          virtual.data0=virtual.data0[-which(virtual.data0[,"order.num"]==num),]
+          rownames(training.data0)<-1:nrow(training.data0)
+          rownames(virtual.data0)<-1:nrow(virtual.data0)
+          
+          training.data0.x <- data.frame(training.data0[,keep.x])
+          training.data0.y <-  data.frame(training.data0[,"es"]) 
+          colnames(training.data0.y)="es"
+          virtual.data0.x=as.data.frame(virtual.data0[,keep.x])
+          gp0 = fn.gp(training.data0.x, training.data0.y,virtual.data0.x)
+          
+          EI=fn.utility(data.training,estimator,training.data0,gp0,tar_num)
+          EI=as.data.frame(EI)
+          
+          
+          num= min(virtual.data0[which(EI[,"EI"]==max(EI[,"EI"])),"order.num"])
+          if(u>=nrow(iter)-1)
+            break
+          SD[u+1,]=fn.OC(data.training,training.data0)
+          dis[u+1,]=fn.dis(data.training, training.data0[nrow(training.data0),"es"])
+          
+          if(sum(EI)==0||(nrow(unique(EI))==1&nrow(EI)>1))
+          {
+            SD[c((u+1):(nrow(iter)-1)),]=SD[u+1,]
+            dis[c((u+1):(nrow(iter)-1)),]=dis[u+1,]
+            break
+          }else{ 
+            if(min(abs(training.data0[,"es"]-data.training[tar_num,"es"]))<=0.003*(y_range))
+            {
+              if(u>=nrow(iter)-2){
+                break
+              }else{
+                SD[c((u+1):(nrow(iter)-1)),]=0
+                dis[c((u+1):(nrow(iter)-1)),]=0
+                break
+              }
+            }
+            u=u+1
+          }
+        }
+        data.OC.SD[,i]=SD[,1]
+        data.dis[,i]=dis[,1]
+        
+      } 
+      data.iter.num.max[i,1]=u
+    } 
+  }
+  data.iter.num.max=as.data.frame(data.iter.num.max)
+  iter_mean=cbind(iter_mean,data.iter.num.max)
+  iter_mean_w=cbind(iter_mean_w,data.iter.num.max)
+  iter_mean=as.data.frame(iter_mean[,-1])
+  iter_total[1,2*(estimator-1)+1]=mean(unlist(iter_mean))
+  iter_total[1,2*(estimator-1)+2]=t.test(iter_mean)$conf.int[2]-t.test(iter_mean)$estimate
+  
+  
+  for(j in 1:nrow(data.OC.SD))
+  {
+    ave.OC[j,2*(estimator)-1]=mean(data.OC.SD[j,])
+    if (length(unique(data.OC.SD[j,]))==1)
+      ave.OC[j,2*(estimator)]=0 else
+        ave.OC[j,2*(estimator)]=t.test(data.OC.SD[j,])$conf.int[2]-t.test(data.OC.SD[j,])$estimate
+  }
+
+  
+  for(j in 1:nrow(data.dis))
+  {
+    ave.dis[j,2*(estimator)-1]=mean(data.dis[j,])
+    if (length(unique(data.dis[j,]))==1)
+      ave.dis[j,2*(estimator)]=0 else
+        ave.dis[j,2*(estimator)]=t.test(data.dis[j,])$conf.int[2]-t.test(data.dis[j,])$estimate
+  }
+
+  estimator=estimator+1
+  if(estimator>5)
+    break
+}
+colnames(ave.dis)=c("ego","ego.sd","PI","PI.sd","LCB","LCB.sd","EQI","EQI.sd","MOAF","MOAF.sd")
+write.csv(ave.dis,"ave.dis.csv")
+colnames(ave.OC)=c("ego","ego.sd","PI","PI.sd","LCB","LCB.sd","EQI","EQI.sd","MOAF","MOAF.sd")
+write.csv(ave.OC,"ave.OC.csv")
+colnames(iter_total)=c("ego","ego.sd","PI","PI.sd","LCB","LCB.sd","EQI","EQI.sd","MOAF","MOAF.sd")
+write.csv(iter_total,"iter_total.csv")
+write.csv(iter_mean_w,"iter_mean_w.csv")
+
+end_time<- Sys.time()
+elapsed_time<-end_time-start_time
+cat("运行时长：",elapsed_time,"\n")
+write.csv(elapsed_time,"elapsed_time.csv")
